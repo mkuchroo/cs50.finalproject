930161b6ff13267ab42fdf256bbfc31a1394ace1
Katherine <kdean@college.harvard.edu>
2012-11-29 20:21:06 -0500

    fourth push, I think?

--- project/html/index.php
+++ project/html/index.php
@@ -1,8 +1,13 @@
 <?php
 
     // configuration
-        require("../includes/config.php"); 
-
+    require("../includes/config.php"); 
+?>
+ 
+    <img name="slideImg" src="logo.gif" width = 500 height = 375 border = 0>
+    <body onLoad = "switchImage('slideImg')">
+    
+<?php
     // render portfolio
     render("homepage.php", ["title" => "Harvard Cricket"]);
 
